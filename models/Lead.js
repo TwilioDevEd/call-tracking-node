df6c3e32c8a1d29d99fee13f4a5c4f01f1beab41
Juan Pablo Santos <jsantos@stackbuilders.com>
2015-09-08 09:36:19 -0500

    Lead model

---
+++ models/Lead.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var LeadSchema = new mongoose.Schema({
+    callerNumber: {type: String, required: true},
+    callSid: {type: String, required: true},
+    city: {type: String, required: false},
+    state: {type: String, required: false},
+    callerName: {type: String, required: false}
+});
+
+// Create a Mongoose model from our schema
+var Lead = mongoose.model('Lead', LeadSchema);
+
+// export model as our module interface
+module.exports = Lead;
