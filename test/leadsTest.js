03f4d8b6fdc68409f9adc91dc7e1f4d7778f6269
Juan Pablo Santos <jsantos@stackbuilders.com>
2015-09-10 15:31:35 -0500

    Add tests for statistics

--- test/leadsTest.js
+++ test/leadsTest.js
@@ -2,6 +2,7 @@ require('./testHelper');
 
 var supertest = require('supertest');
 var expect = require('chai').expect;
+var q = require('q');
 
 var app = require('../webapp');
 var config = require('../config');
@@ -90,6 +91,79 @@ describe('Lead controllers', function() {
                         .expect(200, done);
                 });
 
+        });
+    });
+
+    describe('statistics for pie charts', function() {
+        var leadSourceOneNumber = '+149372894';
+        var leadSourceOneDescription = 'Some description';
+        var leadSourceOneForwardingNumber = '+132947845';
+
+        var leadSourceTwoNumber = '+149343243';
+        var leadSourceTwoDescription = 'Some other description';
+        var leadSourceTwoForwardingNumber = '+193274345';
+
+        var callerNumber = '+1248623948';
+        var callSid = '5up3runiqu3c411s1d';
+
+        it('responds with statistics for a leads by lead source chart', function(done) {
+            var newLeadSourceOne =
+                    new LeadSource({number: leadSourceOneNumber,
+                                    description: leadSourceOneDescription,
+                                    forwardingNumber: leadSourceOneForwardingNumber
+                                   }).save();
+            var newLeadSourceTwo =
+                    new LeadSource({number: leadSourceTwoNumber,
+                                    description: leadSourceTwoDescription,
+                                    forwardingNumber: leadSourceTwoForwardingNumber
+                                   }).save();
+
+            var newLead = new Lead({callerNumber: callerNumber, callSid: callSid});
+
+            q.all([newLeadSourceOne, newLeadSourceTwo])
+                .then(function(savedLeadSources) {
+                    var leadSourceOne = savedLeadSources[0];
+                    var leadSourceTwo = savedLeadSources[1];
+
+                    newLead.leadSource = leadSourceOne._id;
+                    return newLead.save();
+                })
+                .then(function(savedLead) {
+                    var agent = supertest(app);
+                    agent.get('/lead/summary-by-lead-source')
+                        .expect(function(response) {
+                            expect(response.text).to.equal("{\"Some description\":1}");
+                        })
+                        .expect(200, done);
+                })
+                .catch(function(err) {
+                    console.log(err);
+                    done();
+                });
+        });
+
+        it('responds with statistics for a leads by city chart', function(done) {
+            var newLeadSource =
+                    new LeadSource({number: leadSourceOneNumber,
+                                    description: leadSourceOneDescription,
+                                    forwardingNumber: leadSourceOneForwardingNumber
+                                   }).save();
+
+            var newLead = new Lead({callerNumber: callerNumber, callSid: callSid, city: 'The moon'});
+
+            newLeadSource
+                .then(function(savedLeadSource) {
+                    newLead.leadSource = savedLeadSource._id;
+                    return newLead.save();
+                })
+                .then(function(savedLead) {
+                    var agent = supertest(app);
+                    agent.get('/lead/summary-by-city')
+                        .expect(function(response) {
+                            expect(response.text).to.equal("{\"The moon\":1}");
+                        })
+                        .expect(200, done);
+                });
         });
     });
 
