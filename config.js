1a6f89a98db757098b1de680709b7085d86ea190
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-18 15:16:24 -0600

    Cleanup config.js

--- config.js
+++ config.js
@@ -1,22 +1,18 @@
 // Do not allow the application to run if env vars not set in .env file
 require('dotenv-safe').load();
 
-var cfg = {};
+var env = process.env;
 
-// HTTP Port to run our web application
-cfg.port = process.env.PORT || 3000;
-
-// A random string that will help generate secure one-time passwords and
-// HTTP sessions
-cfg.secret = process.env.APP_SECRET || 'keyboard cat';
-
-cfg.accountSid = process.env.TWILIO_ACCOUNT_SID;
-cfg.authToken = process.env.TWILIO_AUTH_TOKEN;
-cfg.appSid = process.env.TWILIO_APP_SID;
-
-// MongoDB connection string - MONGO_URL is for local dev,
-// MONGOLAB_URI is for the MongoLab add-on for Heroku deployment
-cfg.mongoUrl = process.env.MONGOLAB_URI || process.env.MONGO_URL;
-
-// Export configuration object
-module.exports = cfg;
+module.exports = {
+  // HTTP Port to run our web application
+  port: env.port || 3000,
+  // A random string that will help generate secure one-time passwords and
+  // HTTP sessions
+  secret: env.APP_SECRET || 'keyboard cat',
+  accountSid: env.TWILIO_ACCOUNT_SID,
+  authToken: env.TWILIO_AUTH_TOKEN,
+  appSid: env.TWILIO_APP_SID,
+  // MongoDB connection string - MONGO_URL is for local dev,
+  // MONGOLAB_URI is for the MongoLab add-on for Heroku deployment
+  mongoUrl: process.env.MONGOLAB_URI || process.env.MONGO_URL,
+};
